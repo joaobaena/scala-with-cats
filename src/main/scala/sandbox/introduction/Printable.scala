c66edce71218fc0f765800e8d36374082b0aa5c7
Paulo Baena <jpbaena@gmail.com>
2019-03-01 22:38:10 +0100

    Exercise 1.5: Equality, Liberty, and Felinity

--- src/main/scala/sandbox/introduction/Printable.scala
+++ src/main/scala/sandbox/introduction/Printable.scala
@@ -37,8 +37,7 @@ object PrintableTest extends App {
   Cat("Pink", 4, "white").print
 }
 
-
-object PrintableWithCats extends App {
+object ShowWithCats extends App {
 
   import cats.Show
   import cats.instances.int._
@@ -49,4 +48,24 @@ object PrintableWithCats extends App {
   val showString: Show[String] = Show.apply[String]
   implicit val showCat: Show[Cat] = Show.show(value => s"${value.name} is a ${value.age} year-old ${value.color} cat.")
   println(Cat("Pink", 4, "white").show)
+}
+
+object EqWithCats extends App {
+
+  import cats.Eq
+  import cats.syntax.eq._
+  import cats.instances.option._
+
+  implicit val eqCat: Eq[Cat] = Eq.instance((c1, c2) => c1.name == c2.name && c1.age == c2.age && c1.color == c2.color)
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+
+  assert(cat1 =!= cat2, "are equal")
+
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+
+  assert(optionCat1 =!= optionCat2, "are equal")
+  assert(optionCat1 === optionCat1, "are not equal")
+
 }
